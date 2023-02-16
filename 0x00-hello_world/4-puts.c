517c78f7aad67fda3e8311ad41cfb3af46f95b86
Abeltsf4 <abeltsf4@gmail.com>
2023-02-16 09:40:51 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a c program that pritns an output using put function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
