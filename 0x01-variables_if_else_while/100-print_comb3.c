04e167c1b32bb22b6fb24b40c0155e3278dc049b
Abeltsf4 <abeltsf4@gmail.com>
2023-02-17 11:45:29 -0800

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int s = 0, e = 1, s1 = 1, e1 = 2;
+
+	while (s <= e)
+	{
+		s1 = s + 1;
+		while (s1 <= e1)
+		{
+			putchar(s + '0');
+			putchar(s1 + '0');
+			if (s != e)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			s1++;
+		}
+		s++;
+	}
+
+	return (0);
+}
