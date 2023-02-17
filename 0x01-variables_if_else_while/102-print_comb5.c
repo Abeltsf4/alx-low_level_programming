acb07a69cda3392752fb11a63fb51b4bae13a4e6
Abeltsf4 <abeltsf4@gmail.com>
2023-02-17 13:14:38 -0800

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 			putchar(num1 < 9 ? 0 + '0' : (num1 / 10) + '0');
 			putchar(num1 < 9 ? num1 + '0' : (num1 % 10) + '0');
 			putchar(' ');
-			putchar(num2 < 9 ? 0 + '0' : (num2 % 10) + '0');
+			putchar(num2 < 9 ? 0 + '0' : (num2 / 10) + '0');
 			putchar(num2 < 9 ? num2 + '0' : (num2 % 10) + '0');
 			if (num1 != num1max)
 			{
