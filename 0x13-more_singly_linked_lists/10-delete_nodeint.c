10ed1af4fa2370dc73399a2abc5178e738976e9c
Abeltsf4 <abeltsf4@gmail.com>
2023-04-03 17:23:53 -0700

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,14 +24,18 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	while (i < index - 1)
 	{
 		if (!temp || !(temp->next)
-				return (-1);
+				{
 				temp = temp->next;
-				i++;
+				return (-1);
 				}
-
+				i++;
+				else
+				{
 				current = temp->next;
 				temp->next = current->next;
 				free(current);
 
 				return (1);
+				}
+	}
 }
